3bb97efea6612fb54ce9080e66798ea75f19fdbd
Priyanshu kumar <pk6346198@gmail.com>
2025-09-29 23:33:24 +0530

    Create db.js

---
+++ backend/config/db.js
@@ -0,0 +1,19 @@
+// backend/config/db.js
+
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (err) {
+    console.error(`Error connecting to MongoDB: ${err.message}`);
+    process.exit(1); // Exit process with failure
+  }
+};
+
+module.exports = connectDB;
